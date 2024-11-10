2b5ea63ee09d33a0f3aaa5ce6206af19ab2dabae
Shaxriyor <shaxriyorturgunboyovshaxriyor@gmail.com>
2024-11-10 20:31:46 +0500

    3

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('btn-8');
+
+btn.addEventListener('click' , ()=>{
+    setTimeout(() => {
+         window.location.href = 'pages/create.ommabop.html'
+    }, 500);
+})
